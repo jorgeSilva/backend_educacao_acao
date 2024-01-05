e9633714f8e8ac4d0751e4e6240c4f0c89147f17
jorgeSilva <jorgelfsilva5@gmail.com>
2024-01-04 23:55:27 -0300

    fix-fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const server = express()
 require('dotenv').config()
-// fix
+// fix-fix
 
 server.use(cors())
 server.use(express.json())
